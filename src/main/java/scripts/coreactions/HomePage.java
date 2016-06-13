50bd73e46a731f19e942fb2cf193a84a6c42d060
Nguyen Thi Hang <nguyenhang1611@gmail.com>
2016-06-13 19:11:26 +0700

    bai tap thuc hanh day 4

---
+++ src/main/java/scripts/coreactions/HomePage.java
@@ -0,0 +1,40 @@
+package main.java.scripts.coreactions;
+
+import main.java.scripts.coreactions.CoreActionForm;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * HomePage class
+ * 
+ * @author HangNT
+ * @since 2016/06/09
+ */
+public class HomePage extends CoreActionForm {
+	public HomePage(WebDriver driver) {
+		super(driver);
+	}
+
+	public static String accountLinkText = "css=a.skip-link.skip-account";
+	public static String registerLinkText = "xpath=//*[@id='header-account']//ul/li[5]/a";
+
+	/**
+	 * 
+	 * @author HangNT
+	 * @since 2016/06/09
+	 * @param url
+	 */
+	public static void openUrl(String url) {
+		CoreActionForm objCommon = new CoreActionForm(driver);
+		objCommon.openThePage(url);
+	}
+
+	/**
+	 * Close page
+	 * 
+	 * @author HangNT
+	 * @since 106/06/09
+	 */
+	public static void closePage() {
+		driver.close();
+	}
+}
